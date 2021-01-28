dmcilvaney/CBL-Mariner

Make device mapper roots more flexible & reliable

toolkit/tools/imagegen/configuration/partitionflag.go
@@ -1,0 +1,70 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Parser for the image builder's configuration schemas.
+
+package configuration
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// PartitionFlag describes the features of a partition
+type PartitionFlag string
+
+const (
+	// PartitionFlagESP indicates this is the UEFI esp partition
+	PartitionFlagESP PartitionFlag = "esp"
+	// PartitionFlagGrub indicates this is a grub boot partition
+	PartitionFlagGrub PartitionFlag = "grub"
+	// PartitionFlagBiosGrub indicates this is a bios grub boot partition
+	PartitionFlagBiosGrub PartitionFlag = "bios_grub"
+	// PartitionFlagBoot indicates this is a boot partition
+	PartitionFlagBoot PartitionFlag = "boot"
+	// PartitionFlagDeviceMapperRoot indicates this partition will be used for a device mapper root device
+	PartitionFlagDeviceMapperRoot PartitionFlag = "dmroot"
+)
+
+func (p PartitionFlag) String() string {
+	return fmt.Sprint(string(p))
+}
+
+// GetValidPartitionFlags returns a list of all the supported
+// partition flags
+func (p *PartitionFlag) GetValidPartitionFlags() (types []PartitionFlag) {
+	return []PartitionFlag{
+		PartitionFlagESP,
+		PartitionFlagGrub,
+		PartitionFlagBiosGrub,
+		PartitionFlagBoot,
+		PartitionFlagDeviceMapperRoot,
+	}
+}
+
+// IsValid returns an error if the VerityErrorBehavior is not valid
+func (p *PartitionFlag) IsValid() (err error) {
+	for _, valid := range p.GetValidPartitionFlags() {
+		if *p == valid {
+			return
+		}
+	}
+	return fmt.Errorf("invalid value for Flag (%s)", p)
+}
+
+// UnmarshalJSON Unmarshals an PartitionFlag entry
+func (p *PartitionFlag) UnmarshalJSON(b []byte) (err error) {
+	// Use an intermediate type which will use the default JSON unmarshal implementation
+	type IntermediateTypePartitionFlag PartitionFlag
+	err = json.Unmarshal(b, (*IntermediateTypePartitionFlag)(p))
+	if err != nil {
+		return fmt.Errorf("failed to parse [PartitionFlag]: %w", err)
+	}
+
+	// Now validate the resulting unmarshaled object
+	err = p.IsValid()
+	if err != nil {
+		return fmt.Errorf("failed to parse [PartitionFlag]: %w", err)
+	}
+	return
+}

toolkit/tools/imagegen/configuration/partitionflag_test.go
@@ -1,0 +1,79 @@
+// Copyright Microsoft Corporation.
+// Licensed under the MIT License.
+
+package configuration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//TestMain found in configuration_test.go.
+
+var (
+	validPartitionFlags = []PartitionFlag{
+		PartitionFlag("esp"),
+		PartitionFlag("grub"),
+		PartitionFlag("bios_grub"),
+		PartitionFlag("boot"),
+		PartitionFlag("dmroot"),
+	}
+	invalidPartitionFlag     = PartitionFlag("not_a_partition_flag")
+	validPartitionFlagJSON   = `"esp"`
+	invalidPartitionFlagJSON = `1234`
+)
+
+func TestShouldSucceedValidFlagsMatch_PartitionFlag(t *testing.T) {
+	var pf PartitionFlag
+	assert.Equal(t, len(validPartitionFlags), len(pf.GetValidPartitionFlags()))
+
+	for _, partitionFlag := range validPartitionFlags {
+		found := false
+		for _, validPartitionType := range pf.GetValidPartitionFlags() {
+			if partitionFlag == validPartitionType {
+				found = true
+			}
+		}
+		assert.True(t, found)
+	}
+}
+
+func TestShouldSucceedParsingValidPartitionFlag_PartitionFlag(t *testing.T) {
+	for _, validPartitionFlag := range validPartitionFlags {
+		var checkedPartitionFlag PartitionFlag
+
+		assert.NoError(t, validPartitionFlag.IsValid())
+		err := remarshalJSON(validPartitionFlag, &checkedPartitionFlag)
+		assert.NoError(t, err)
+		assert.Equal(t, validPartitionFlag, checkedPartitionFlag)
+	}
+}
+
+func TestShouldFailParsingInvalidPartitionFlag_PartitionFlag(t *testing.T) {
+	var checkedPartitionFlag PartitionFlag
+
+	err := invalidPartitionFlag.IsValid()
+	assert.Error(t, err)
+	assert.Equal(t, "invalid value for Flag (not_a_partition_flag)", err.Error())
+
+	err = remarshalJSON(invalidPartitionFlag, &checkedPartitionFlag)
+	assert.Error(t, err)
+	assert.Equal(t, "failed to parse [PartitionFlag]: invalid value for Flag (not_a_partition_flag)", err.Error())
+}
+
+func TestShouldSucceedParsingValidJSON_PartitionFlag(t *testing.T) {
+	var checkedPartitionFlag PartitionFlag
+
+	err := marshalJSONString(validPartitionFlagJSON, &checkedPartitionFlag)
+	assert.NoError(t, err)
+	assert.Equal(t, validPartitionFlags[0], checkedPartitionFlag)
+}
+
+func TestShouldFailParsingInvalidJSON_PartitionFlag(t *testing.T) {
+	var checkedPartitionFlag PartitionFlag
+
+	err := marshalJSONString(invalidPartitionFlagJSON, &checkedPartitionFlag)
+	assert.Error(t, err)
+	assert.Equal(t, "failed to parse [PartitionFlag]: json: cannot unmarshal number into Go value of type configuration.IntermediateTypePartitionFlag", err.Error())
+}

cilegordev/Azure-Linux

toolkit/tools/imagegen/configuration/partitionflag.go
@@ -1,0 +1,70 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Parser for the image builder's configuration schemas.
+
+package configuration
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// PartitionFlag describes the features of a partition
+type PartitionFlag string
+
+const (
+	// PartitionFlagESP indicates this is the UEFI esp partition
+	PartitionFlagESP PartitionFlag = "esp"
+	// PartitionFlagGrub indicates this is a grub boot partition
+	PartitionFlagGrub PartitionFlag = "grub"
+	// PartitionFlagBiosGrub indicates this is a bios grub boot partition
+	PartitionFlagBiosGrub PartitionFlag = "bios_grub"
+	// PartitionFlagBoot indicates this is a boot partition
+	PartitionFlagBoot PartitionFlag = "boot"
+	// PartitionFlagDeviceMapperRoot indicates this partition will be used for a device mapper root device
+	PartitionFlagDeviceMapperRoot PartitionFlag = "dmroot"
+)
+
+func (p PartitionFlag) String() string {
+	return fmt.Sprint(string(p))
+}
+
+// GetValidPartitionFlags returns a list of all the supported
+// partition flags
+func (p *PartitionFlag) GetValidPartitionFlags() (types []PartitionFlag) {
+	return []PartitionFlag{
+		PartitionFlagESP,
+		PartitionFlagGrub,
+		PartitionFlagBiosGrub,
+		PartitionFlagBoot,
+		PartitionFlagDeviceMapperRoot,
+	}
+}
+
+// IsValid returns an error if the PartitionFlag is not valid
+func (p *PartitionFlag) IsValid() (err error) {
+	for _, valid := range p.GetValidPartitionFlags() {
+		if *p == valid {
+			return
+		}
+	}
+	return fmt.Errorf("invalid value for Flag (%s)", p)
+}
+
+// UnmarshalJSON Unmarshals an PartitionFlag entry
+func (p *PartitionFlag) UnmarshalJSON(b []byte) (err error) {
+	// Use an intermediate type which will use the default JSON unmarshal implementation
+	type IntermediateTypePartitionFlag PartitionFlag
+	err = json.Unmarshal(b, (*IntermediateTypePartitionFlag)(p))
+	if err != nil {
+		return fmt.Errorf("failed to parse [Flag]: %w", err)
+	}
+
+	// Now validate the resulting unmarshaled object
+	err = p.IsValid()
+	if err != nil {
+		return fmt.Errorf("failed to parse [Flag]: %w", err)
+	}
+	return
+}

toolkit/tools/imagegen/configuration/partitionflag_test.go
@@ -1,0 +1,79 @@
+// Copyright Microsoft Corporation.
+// Licensed under the MIT License.
+
+package configuration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//TestMain found in configuration_test.go.
+
+var (
+	validPartitionFlags = []PartitionFlag{
+		PartitionFlag("esp"),
+		PartitionFlag("grub"),
+		PartitionFlag("bios_grub"),
+		PartitionFlag("boot"),
+		PartitionFlag("dmroot"),
+	}
+	invalidPartitionFlag     = PartitionFlag("not_a_partition_flag")
+	validPartitionFlagJSON   = `"esp"`
+	invalidPartitionFlagJSON = `1234`
+)
+
+func TestShouldSucceedValidFlagsMatch_PartitionFlag(t *testing.T) {
+	var pf PartitionFlag
+	assert.Equal(t, len(validPartitionFlags), len(pf.GetValidPartitionFlags()))
+
+	for _, partitionFlag := range validPartitionFlags {
+		found := false
+		for _, validPartitionType := range pf.GetValidPartitionFlags() {
+			if partitionFlag == validPartitionType {
+				found = true
+			}
+		}
+		assert.True(t, found)
+	}
+}
+
+func TestShouldSucceedParsingValidPartitionFlag_PartitionFlag(t *testing.T) {
+	for _, validPartitionFlag := range validPartitionFlags {
+		var checkedPartitionFlag PartitionFlag
+
+		assert.NoError(t, validPartitionFlag.IsValid())
+		err := remarshalJSON(validPartitionFlag, &checkedPartitionFlag)
+		assert.NoError(t, err)
+		assert.Equal(t, validPartitionFlag, checkedPartitionFlag)
+	}
+}
+
+func TestShouldFailParsingInvalidPartitionFlag_PartitionFlag(t *testing.T) {
+	var checkedPartitionFlag PartitionFlag
+
+	err := invalidPartitionFlag.IsValid()
+	assert.Error(t, err)
+	assert.Equal(t, "invalid value for Flag (not_a_partition_flag)", err.Error())
+
+	err = remarshalJSON(invalidPartitionFlag, &checkedPartitionFlag)
+	assert.Error(t, err)
+	assert.Equal(t, "failed to parse [Flag]: invalid value for Flag (not_a_partition_flag)", err.Error())
+}
+
+func TestShouldSucceedParsingValidJSON_PartitionFlag(t *testing.T) {
+	var checkedPartitionFlag PartitionFlag
+
+	err := marshalJSONString(validPartitionFlagJSON, &checkedPartitionFlag)
+	assert.NoError(t, err)
+	assert.Equal(t, validPartitionFlags[0], checkedPartitionFlag)
+}
+
+func TestShouldFailParsingInvalidJSON_PartitionFlag(t *testing.T) {
+	var checkedPartitionFlag PartitionFlag
+
+	err := marshalJSONString(invalidPartitionFlagJSON, &checkedPartitionFlag)
+	assert.Error(t, err)
+	assert.Equal(t, "failed to parse [Flag]: json: cannot unmarshal number into Go value of type configuration.IntermediateTypePartitionFlag", err.Error())
+}