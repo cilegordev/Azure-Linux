microsoft/azurelinux

azurelinux-release: use consistent creation of here documents

SPECS/azurelinux-release/azurelinux-release.spec
@@ -41,11 +41,13 @@ Azure Linux release files such as yum configs and other %{_sysconfdir}/ release 
 install -d %{buildroot}%{_sysconfdir}
 install -d %{buildroot}/%{_libdir}
 
-echo "Azure Linux %{dist_version}" > %{buildroot}%{_libdir}/azurelinux-release
-echo "AZURELINUX_BUILD_NUMBER=%{azurelinux_build_number}" >> %{buildroot}%{_libdir}/azurelinux-release
+cat <<-"EOF" > %{buildroot}%{_libdir}/azurelinux-release
+Azure Linux %{dist_version}
+AZURELINUX_BUILD_NUMBER=%{azurelinux_build_number}
+EOF
 ln -sv ..%{_libdir}/azurelinux-release %{buildroot}%{_sysconfdir}/azurelinux-release
 
-cat > %{buildroot}%{_libdir}/lsb-release <<- "EOF"
+cat <<-"EOF" > %{buildroot}%{_libdir}/lsb-release
 DISTRIB_ID="azurelinux"
 DISTRIB_RELEASE="%{dist_version}"
 DISTRIB_CODENAME=AzureLinux
@@ -53,7 +55,7 @@ DISTRIB_DESCRIPTION="%{dist_name} %{dist_version}"
 EOF
 ln -sv ..%{_libdir}/lsb-release %{buildroot}%{_sysconfdir}/lsb-release
 
-cat > %{buildroot}/%{_libdir}/os-release << EOF
+cat <<-"EOF" > os-release
 NAME="%{dist_name}"
 VERSION="%{dist_version}"
 ID=azurelinux
@@ -66,12 +68,12 @@ SUPPORT_URL="%{url}"
 EOF
 ln -sv ..%{_libdir}/os-release %{buildroot}%{_sysconfdir}/os-release
 
-cat > %{buildroot}%{_libdir}/issue <<- EOF
+cat <<-"EOF" > %{buildroot}%{_libdir}/issue
 Welcome to Azure Linux %{dist_version} (%{_arch}) - (\l)
 EOF
 ln -sv ..%{_libdir}/issue %{buildroot}%{_sysconfdir}/issue
 
-cat > %{buildroot}%{_libdir}/issue.net <<- EOF
+cat <<-"EOF" > %{buildroot}%{_libdir}/issue.net
 Welcome to Azure Linux %{dist_version} (%{_arch})
 EOF
 ln -sv ..%{_libdir}/issue.net %{buildroot}%{_sysconfdir}/issue.net

cilegordev/Azure-Linux

SPECS/azurelinux-release/azurelinux-release.spec
@@ -23,11 +23,13 @@ Azure Linux release files such as dnf configs and other %{_sysconfdir}/ release 
 install -d %{buildroot}%{_sysconfdir}
 install -d %{buildroot}%{_libdir}
 
-echo "%{distribution} %{version}" > %{buildroot}%{_libdir}/azurelinux-release
-echo "AZURELINUX_BUILD_NUMBER=%{mariner_build_number}" >> %{buildroot}%{_libdir}/azurelinux-release
+cat <<-"EOF" > %{buildroot}%{_libdir}/azurelinux-release
+%{distribution} %{version}
+AZURELINUX_BUILD_NUMBER=%{mariner_build_number}
+EOF
 ln -sv ..%{_libdir}/azurelinux-release %{buildroot}%{_sysconfdir}/azurelinux-release
 
-cat > %{buildroot}%{_libdir}/lsb-release <<- "EOF"
+cat <<-"EOF" > %{buildroot}%{_libdir}/lsb-release
 DISTRIB_ID="azurelinux"
 DISTRIB_RELEASE="%{version}"
 DISTRIB_CODENAME=AzureLinux
@@ -35,7 +37,7 @@ DISTRIB_DESCRIPTION="%{distribution} %{version}"
 EOF
 ln -sv ..%{_libdir}/lsb-release %{buildroot}%{_sysconfdir}/lsb-release
 
-cat > %{buildroot}%{_libdir}/os-release << EOF
+cat <<-"EOF" > %{buildroot}%{_libdir}/os-release
 NAME="%{distribution}"
 VERSION="%{version}"
 ID=azurelinux
@@ -48,12 +50,12 @@ SUPPORT_URL="%{url}"
 EOF
 ln -sv ..%{_libdir}/os-release %{buildroot}%{_sysconfdir}/os-release
 
-cat > %{buildroot}%{_libdir}/issue <<- EOF
+cat <<-"EOF" > %{buildroot}%{_libdir}/issue
 Welcome to %{distribution} %{version} (%{_arch}) - (\l)
 EOF
 ln -sv ..%{_libdir}/issue %{buildroot}%{_sysconfdir}/issue
 
-cat > %{buildroot}%{_libdir}/issue.net <<- EOF
+cat <<-"EOF" > %{buildroot}%{_libdir}/issue.net
 Welcome to %{distribution} %{version} (%{_arch})
 EOF
 ln -sv ..%{_libdir}/issue.net %{buildroot}%{_sysconfdir}/issue.net
@@ -76,6 +78,7 @@ ln -sv ..%{_libdir}/issue.net %{buildroot}%{_sysconfdir}/issue.net
 - remove %%config(noreplace) from *-release files
 - define dist_version and use local macros
 - move *-release and issue files under %%_libdir and make %%_sysconfdir symlinks
+- use consistent creation of here documents
 
 * Thu Feb 01 2024 Mykhailo Bykhovtsev <mbykhovtsev@microsoft.com> - 3.0-3
 - Renamed mariner-release to azurelinux-release file